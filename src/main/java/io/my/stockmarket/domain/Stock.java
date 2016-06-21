edd5b91e74a94d5cc24385b5059ce9b6dbcb8f22
Ego <lalala@fakemail.kom>
2016-06-21 23:19:57 +0300

    Domain

---
+++ src/main/java/io/my/stockmarket/domain/Stock.java
@@ -0,0 +1,12 @@
+package io.my.stockmarket.domain;
+
+/**
+ * Created by ego on 6/21/16.
+ */
+public class Stock {
+
+    private String id;
+    private String name;
+    private StockType stockType;
+
+}
---
+++ src/main/java/io/my/stockmarket/domain/StockType.java
@@ -0,0 +1,9 @@
+package io.my.stockmarket.domain;
+
+/**
+ * Created by ego on 6/21/16.
+ */
+public enum StockType {
+
+    COMMON, PREFERRED;
+}
