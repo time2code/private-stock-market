8eefd7100fbc2b63c49cd5efdced66e2b07f07dc
Ego <lalala@fakemail.kom>
2016-06-22 00:02:12 +0300

    CDI Weld

--- src/main/java/io/my/StockMarket.java
+++ src/main/java/io/my/StockMarket.java
@@ -1,13 +1,19 @@
 package io.my;
 
+import org.jboss.weld.environment.se.bindings.Parameters;
+import org.jboss.weld.environment.se.events.ContainerInitialized;
+
+import javax.enterprise.event.Observes;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class StockMarket
 {
-    public static void main( String[] args )
+    public void openMarket(@Observes ContainerInitialized event, @Parameters List<String> params)
     {
-        System.out.println( "Hello Stock Market!" );
+        System.out.println( "Hello Stock Market! " + params.size() );
     }
 }
